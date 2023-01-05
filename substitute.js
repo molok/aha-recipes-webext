0a722bd2e8c415f7b8f9430d34d78285ad78bc58
Alessio Bolognino <alessio.bolognino@gmail.com>
2023-01-05 17:24:44 +0100

    removed old

--- substitute.js
+++ substitute.js
@@ -45,7 +45,7 @@ function normalizeQty(x) {
 
 /**
  *
- * @param malts {Element[]}
+ * @param inputMalts {Element[]}
  */
 function rewriteMalts(inputMalts) {
   const malts = inputMalts.map(m => ({element: m, origContent: m.textContent}))
@@ -84,30 +84,6 @@ function rewriteGramsPerL(inputHops, batchSizeL, icon) {
 }
 
 
-function transformMalts(input) {
-  let lines = input.split("\n").map((x, idx) => ({id: idx, origContent: x}))
-  let maltsStart = lines.filter(x => x.origContent.toUpperCase().includes("MALTS") || x.origContent.toUpperCase().includes("FERMENTABLES")).map(x => x.id)[0]
-  if (maltsStart === undefined || maltsStart === null) {
-    maltsStart = lines.filter(x => x.origContent.toUpperCase().includes("INGREDIENTS")).map(x => x.id)[0]
-  }
-  const maltsEnd = lines.filter(x => x.id > maltsStart).filter(x =>
-      x.origContent.includes("<strong>") ||
-      (x.origContent.replaceAll(/<[^>]*>/g, '').trim().length >= 4 && x.origContent.replaceAll(/<[^>]*>/g, '').toUpperCase() === x.origContent.replaceAll(/<[^>]*>/g, '')) ||
-      x.origContent.includes("hops") || x.origContent.includes("pellets"))
-      .map(x => x.id)[0]
-
-  const malts = lines.slice(maltsStart +1, maltsEnd)
-      .filter(x => !x.origContent.toUpperCase().includes("YIELD") && !x.origContent.toUpperCase().includes("YEAST") && !x.origContent.toUpperCase().includes("HOPS"))
-  malts.forEach(x => { normalizeQty(x) })
-
-  const tot = malts.filter(x => x.g).map(x => x.g).reduce((acc, curr) => acc + curr, 0)
-  malts.filter(x => x.g).forEach(x => x.perc = round(x.g * 100 / tot, 1))
-  malts.filter(x => x.g).forEach(x => x.newContent = x.content
-      .replace('#QTY#', `🌾 ${x.perc.toFixed(2).padStart(6)}% (${formatGrams(x.g)}) `))
-
-  return lines.map( x => x.newContent || x.origContent).join("\n")
-}
-
 function formatGrams(grams) {
   if (grams > 1000) {
     return `${(grams / 1000).toFixed(2)} kg`
@@ -116,65 +92,6 @@ function formatGrams(grams) {
   }
 }
 
-function transformHops(input, sectionName) {
-  let lines = input.split("\n").map((x, idx) => ({id: idx, origContent: x}))
-  let hopsStart = lines.filter(x => x.origContent.toUpperCase().includes(sectionName)).map(x => x.id)[0]
-
-  if (hopsStart === undefined || hopsStart === null) {
-    return input
-  }
-
-  const hopsEnd = lines.filter(x => x.id > hopsStart).filter(x => x.origContent.includes("<strong>") ||
-      x.origContent.replaceAll(/<[^>]*>/g, '').toUpperCase() === x.origContent.replaceAll(/<[^>]*>/g, '')).map(x => x.id)[0]
-
-  const hops = lines.slice(hopsStart, hopsEnd)
-      .filter(x => !x.origContent.toUpperCase().includes("YIELD")
-          && !x.origContent.toUpperCase().includes("YEAST")
-          && !x.origContent.toUpperCase().includes("MALT")
-      )
-
-  hops.forEach(x => { normalizeQty(x) })
-
-  const batchSizeElement = lines.find(x => x.origContent.includes("Yield"))
-
-  const batchSizeGRaw = (batchSizeElement.origContent.match(/\(?[0-9.]+ (us|u.s.)? ?(gallon|gal)s?\.?\)?/gi)||[])[0]
-  const batchSizeLRaw = (batchSizeElement.origContent.match(/\(?[0-9.]+ (liter|l)s?\.?\)?/gi)||[])[0]
-  let batchSizeL
-  if (batchSizeLRaw) {
-    batchSizeL = +(batchSizeLRaw.match(/[0-9.]+/)[0])
-  } else if (batchSizeGRaw) {
-    batchSizeL = round(usgallons_to_l(+(batchSizeGRaw.match(/[0-9.]+/)[0])), 1)
-  }
-
-  batchSizeElement.newContent = batchSizeElement.origContent.replace(batchSizeGRaw, '#QTY#').replace(batchSizeLRaw, '#QTY#')
-      .replaceAll(/  /g, ' ')
-      .replaceAll(/(#QTY#|#QTY# ){2,}/g, '#QTY#')
-      .replace("#QTY#", `${batchSizeL} L`)
-
-  hops.filter(x => x.g).forEach(x => x.gPerL = round(x.g / batchSizeL, 1))
-  hops.filter(x => x.gPerL).forEach(x => x.newContent = x.content
-      .replace('#QTY#', `🌿 ${x.gPerL.toFixed(2).padStart(3)}g/L (${x.g.toFixed(0)}g) `))
-
-  return lines.map( x => x.newContent || x.origContent).join("\n")
-}
-
-let original
-
-function transformRecipe() {
-  try {
-    const ingredients = document.querySelector(".ingredients");
-    original = ingredients.innerHTML
-
-    const newContent = transformMalts(ingredients.innerHTML)
-    const newContent2 = transformHops(newContent, "HOPS")
-    const newContent3 = transformHops(newContent2, "ADDITIONAL")
-    const newContent4 = transformHops(newContent3, "MISC")
-    const newContent5 = transformHops(newContent4, "PELLETS")
-    ingredients.innerHTML = newContent5
-  } catch (e) {
-    console.error(e)
-  }
-}
 
 function showOriginal() {
   const ingredients = document.querySelector(".ingredients");
